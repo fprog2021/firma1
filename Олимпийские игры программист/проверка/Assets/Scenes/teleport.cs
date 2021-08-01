b68fdfe9cb747d5f53225fb37a1c516239f51d38
fprog2021 <87995038+fprog2021@users.noreply.github.com>
2021-08-01 20:39:24 +0300

    новая версия

---
+++ Олимпийские игры программист/проверка/Assets/Scenes/teleport.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class teleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void v1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void v2()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void v3()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void v4()
+    {
+        SceneManager.LoadScene(2);
+    }
+    
+    
+}
